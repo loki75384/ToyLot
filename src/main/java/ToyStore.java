74c2edb109cebd2b371d97c66723f18b100208a1
loki47 <larsentuta49@gmail.com>
2024-02-29 21:53:57 +0300

    ToyStore class add

---
+++ src/main/java/ToyStore.java
@@ -0,0 +1,49 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.PriorityQueue;
+
+public class ToyStore {
+    private PriorityQueue<Toy> toyQueue = new PriorityQueue<>((t1, t2) -> Double.compare(t2.getFrequency(), t1.getFrequency()));
+
+    public void addToy(int id, String name, int quantity, double frequency) {
+        for (int i = 0; i < quantity; i++) {
+            Toy toy = new Toy(id, name, quantity, frequency);
+            toyQueue.add(toy);
+        }
+    }
+
+    public void updateFrequency(int id, double frequency) {
+        for (Toy toy : toyQueue) {
+            if (toy.getId() == id) {
+                toy.setFrequency(frequency);
+                break;
+            }
+        }
+    }
+
+    public void drawToys(int times) {
+        try (FileWriter writer = new FileWriter("result.txt")) {
+            for (int i = 0; i < times; i++) {
+                Toy drawnToy = toyQueue.poll();
+                if (drawnToy != null && drawnToy.getQuantity() > 0) {
+                    decreaseQuantityById(drawnToy.getId());
+                    writer.write("Выбранная игрушка: " + drawnToy.getName() + "\n");
+                } else {
+                    writer.write("Нет доступных игрушек\n");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void decreaseQuantityById(int id) {
+        for (Toy toy : toyQueue) {
+            if (toy.getId() == id) {
+                toy.decreaseQuantity();
+                break;
+            }
+        }
+    }
+}
+
